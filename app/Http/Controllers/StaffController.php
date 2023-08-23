1e442d4ec02816149ece25f82dbd0aae3a19d7c1
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2023-08-23 22:37:26 +0300

    edit routes

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -9,4 +9,13 @@ class StaffController extends Controller
     public function staff_login(){
         return view('receptionest_login');
     }
+
+    public function addStaff(){
+        return view('add_staff');
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/',[WelcomeController::class,'welcome'])->name('welcome');
 Route::get('/home', [PatientController::class, 'home'])->name('home');
 Route::get('/login', [PatientController::class, 'login'])->name('login');
 Route::get('/signup', [PatientController::class, 'signup'])->name('signup');
@@ -16,19 +17,8 @@ Route::get('/booking', [AppointmentController::class, 'booking'])->name('booking
 Route::get('/admin', [AdminController::class, 'login_Admin'])->name('login_Admin');
 Route::get('/services', [ServiceController::class, 'services'])->name('services');
 Route::get('/addService', [ServiceController::class, 'Add_service'])->name('Add_service');
-Route::get('/addStaff', [StaffController::class, 'staff_login'])->name('staff_login');
+Route::get('/receptionest', [StaffController::class, 'staff_login'])->name('staff_login');
+Route::get('/dashboard', [StaffController::class, 'dashboard'])->name('dashboard');
+Route::get('/add_staff', [StaffController::class, 'addStaff'])->name('addStaff');
 Route::get('/doctors', [DoctorController::class, 'Doctors'])->name('Doctors');
 Route::get('/addDoctor', [DoctorController::class, 'Add_Doctor'])->name('Add_Doctor');
-
-
-Route::get('/receptionest', function () {
-    return view('receptionest_login');
-})->name('receptionest');
-
-Route::get('/welcome', function () {
-    return view('welcome');
-})->name('welcome');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
