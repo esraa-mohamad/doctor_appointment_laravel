126273e185b1b43dc0874a7889a545e56776a389
rodinaibrahim77 <rodinaibrahim434@gmail.com>
2023-08-26 22:22:24 +0300

    dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,111 +5,63 @@
     <!--Main Navigation-->
 <header>
     <!-- Sidebar -->
-    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-dark">
-      <div class="position-sticky bg-dark">
+    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse">
+      <div class="position-sticky">
         <div class="list-group list-group-flush mx-3 mt-4 bg-secondary">
           <a
             href="#"
-            class="list-group-item list-group-item-action py-2 ripple bg-dark text-white"
+            class="list-group-item list-group-item-action py-2 ripple text-white"
             aria-current="true"
           >
           <i class="bi bi-clipboard2-pulse-fill"></i><span>patient</span>
           </a>
-          <a href="#" class="list-group-item list-group-item-action py-2 ripple bg-dark text-white">
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple text-white">
             <i class="bi bi-microsoft-teams"></i></i><span>Our Staff</span>
           </a>
-          <a href="#" class="list-group-item list-group-item-action py-2 ripple bg-dark text-white"
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple text-white"
             ><i class="bi bi-capsule-pill"></i><span>Services</span></a
           >
-          <a href="#" class="list-group-item list-group-item-action py-2 ripple bg-dark text-white"
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple text-white"
             ><i class="bi bi-heart-pulse-fill"></i><span>Doctors</span></a
           >
 
         </div>
       </div>
     </nav>
-    <!-- Sidebar -->  
-  
-    <!-- Navbar -->
- 
-    <!-- Navbar -->
-    <div class="container p-3">
-      <button class="btn bg-info text-white mb-3 mt-5">Add +</button>
-    <table class="table align-middle" style="width: 100%; display: block; margin: auto;">
-        <thead>
-          <tr>
-            <th class="bg-dark text-white" scope="col">#</th>
-            <th class="bg-dark text-white" scope="col">Lorem</th>
-            <th class="bg-dark text-white" scope="col">Ipsum</th>
-            <th class="bg-dark text-white" scope="col">Dolor</th>
-            <th class="bg-dark text-white" scope="col">Lorem</th>
-            <th class="bg-dark text-white" scope="col">Ipsum</th>
-            <th class="bg-dark text-white" scope="col">Dolor</th>
-            <th class="bg-dark text-white" scope="col">Lorem</th>
-            <th class="bg-dark text-white" scope="col">Ipsum</th>
-            <th class="bg-dark text-white" scope="col">Dolor</th>
-            <th class="bg-dark text-white" scope="col">Ipsum</th>
-            <th class="bg-dark text-white" scope="col">Dolor</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td scope="row">1</td>
-            <td>Sit</td>
-            <td>Amet</td>
-            <td>Sit</td>
-            <td>Amet</td>
-            <td>Sit</td>
-            <td>Amet</td>
-            <td>Sit</td>
-            <td>Amet</td>
-            <td>Sit</td>
-            <td>Amet</td>
-            <td>
-              <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
-                <i class="fas fa-times"></i>
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td scope="row">2</td>
-            <td>Adipisicing</td>
-            <td>Elit</td>
-            <td>Adipisicing</td>
-            <td>Elit</td>
-            <td>Adipisicing</td>
-            <td>Elit</td>
-            <td>Adipisicing</td>
-            <td>Elit</td>
-            <td>Adipisicing</td>
-            <td>Elit</td>
-            <td>
-              <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
-                <i class="fas fa-times"></i>
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td scope="row">3</td>
-            <td>Hic</td>
-            <td>Fugiat</td>
-            <td>Hic</td>
-            <td>Fugiat</td>
-            <td>Hic</td>
-            <td>Fugiat</td>
-            <td>Hic</td>
-            <td>Fugiat</td>
-            <td>Hic</td>
-            <td>Fugiat</td>
-            <td>
-              <button type="button" class="btn btn-link btn-sm px-3" data-ripple-color="dark">
-                <i class="fas fa-times"></i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      </div>
+
+    <div class="container">
+    <button type="button" id="btt" class="btn btn-info">ADD+</button>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+   
   </header>
   <!--Main Navigation-->
   
